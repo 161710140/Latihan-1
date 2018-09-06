216c33e51e55d73575e75f5df56c48c7b27fd43e
Rastian Fajar <33051573+161710140@users.noreply.github.com>
2018-09-06 16:08:23 +0700

    Update input1.php

--- input1.php
+++ input1.php
@@ -2,7 +2,7 @@
 
 require_once 'Latihan1.php';
 
-$a = new Laptop('Toshiba','Satelite L7440','NVIDIA 520m','2Gb','Intel Core i5');
+$a = new Laptop('Toshiba','Satelite L740','NVIDIA 520m','2Gb','Intel Core i5');
 
 echo "Merk Laptop :".$a->get_merk();
 echo "<br>";
